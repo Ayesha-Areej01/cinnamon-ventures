d893a070b1b5db6e35c5b0483b569b9b053a6038
Ayesha-Areej01 <areej95smiles@gmail.com>
2024-11-23 20:54:03 +0500

    integrated stripe

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,10 +6,13 @@ import Form from "next/form";
 import { PackageIcon, TrolleyIcon } from "@sanity/icons";
 import Image from "next/image";
 import CSV from "../public/CSV.jpg";
+import useBasketStore from "@/app/categories/[slug]/store";
 
 function Header() {
   const { user } = useUser();
   //console.log(user);
+  const itemCount = useBasketStore((state) => 
+  state.items.reduce((total, itemCount) => total + itemCount.quantity, 0));
 
   const createClerkPasskey = async () => {
     try{
@@ -50,6 +53,7 @@ function Header() {
             className="flex-1 relative flex justify-center sm:jutify-start sm:flex-none items-center bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
           >
           <TrolleyIcon className="w-6 h-6" />
+          <span className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">{itemCount}</span>
           <span>My Basket</span>
           </Link>
           <ClerkLoaded>
